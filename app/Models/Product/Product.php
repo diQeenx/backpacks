4dd1b7c1d95493ce1962f4b4b7921ffc6eca6ab6
Dmitriy <danilov.infobel@mail.ru>
2019-12-04 14:44:24 +0300

    added relations to models of product

--- app/Models/Product/Product.php
+++ app/Models/Product/Product.php
@@ -6,5 +6,23 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    //
+    public function details()
+    {
+        return $this->hasMany(ProductDetails::class);
+    }
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function brand()
+    {
+        return $this->belongsTo(Brand::class);
+    }
+
+    public function type()
+    {
+        return $this->belongsTo(Type::class);
+    }
 }
--- app/Models/Product/ProductDetails.php
+++ app/Models/Product/ProductDetails.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class ProductDetails extends Model
 {
-    //
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
+
+    public function color()
+    {
+        return $this->belongsTo(Color::class);
+    }
 }
